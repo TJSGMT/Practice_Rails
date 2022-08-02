c1ae7e97d0343109288eaa15a57dd76d3237ae64
tjs <tejas.g@simformsolutions.com>
2022-08-02 18:13:53 +0530

    commit in cocoon

--- Gemfile
+++ Gemfile
@@ -74,3 +74,5 @@ end
 gem "devise"
 gem "cancancan"
 gem "kaminari"
+gem 'cocoon'
+gem 'jquery-ui-rails'
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+ //= require jquery
+ //= require jquery_ujs
+ //= require jquery-ui/effect-highlight
+ //= require cocoon
+ //= require global
+ //= require turbolinks
